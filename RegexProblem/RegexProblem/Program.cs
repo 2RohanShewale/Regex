29bebc533ff6a442455694c97a72bedb8fb1a9ac
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-01 00:48:47 +0530

    [Rohan]validating first name

---
+++ RegexProblem/RegexProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RegexProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*-----REGEX-----*");
+            User user = new User();
+            user.ValidateFirstName("Rohan");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RegexProblem/RegexProblem/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    internal class User
+    {
+        public void ValidateFirstName(string firstName)
+        {
+            Console.WriteLine(">>>Matching First Name: ");
+            var regex = "^[A-Z][a-z]{2,}$";
+            if (Regex.IsMatch(firstName, regex))
+                Console.WriteLine($"{firstName} is correct input");
+            else
+                Console.WriteLine($"'{firstName}' does not match the pattern");
+            Console.WriteLine();
+        }
+    }
+}
