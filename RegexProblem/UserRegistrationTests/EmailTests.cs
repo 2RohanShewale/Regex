0fdf8b3f1a9aa4497769eaa42894bab82db73716
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-06 17:16:08 +0530

    [Rohan]Add: null and emptystring exceptions for Email

---
+++ RegexProblem/UserRegistrationTests/EmailTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using RegexProblem;
+
+namespace UserRegistrationTests
+{
+    internal class EmailTests
+    {
+        User user = new User();
+        [Test]
+        public void Given_Empty_Email_Should_Throw_UserRegistrationException_Indicating_EmptyInput()
+        {
+            try
+            {
+                user.ValidateEmail("");
+            }
+            catch (UserRegistrationException ex)
+            {
+                Assert.AreEqual("String is empty",ex.Message);
+            }
+        }
+        [Test]
+        public void Given_NULL_Email_Should_Throw_UserRegistrationException()
+        {
+            try
+            {
+                user.ValidateEmail(null);
+            }
+            catch (UserRegistrationException ex)
+            {
+                Assert.AreEqual("Email is null", ex.Message);
+            }
+        }
+    }
+}
