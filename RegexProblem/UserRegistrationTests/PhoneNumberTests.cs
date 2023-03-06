b5763f821d59481961ea26212691e753cd1602c2
Rohan Shewale <shewalerohan50@gmail.com>
2023-03-06 17:06:50 +0530

    [Rohan]Add: null and emptystring exceptions for PhoneNumber

---
+++ RegexProblem/UserRegistrationTests/PhoneNumberTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using RegexProblem;
+
+namespace UserRegistrationTests
+{
+    public class PhoneNumberTests
+    {
+        User user = new User();
+        [Test]
+        public void Given_Empty_Mobile_Should_Throw_UserRegistrationException_Indicating_EmptyInput()
+        {
+            try
+            {
+                user.ValidatePhoneNumber("");
+            }
+            catch (UserRegistrationException ex)
+            {
+                Assert.AreEqual("String is empty", ex.Message);
+            }
+        }
+        [Test]
+        public void Given_NULL_Mobile_Should_Throw_UserRegistrationException()
+        {
+            try
+            {
+                user.ValidatePhoneNumber(null);
+            }
+            catch (UserRegistrationException ex)
+            {
+                Assert.AreEqual("Number is null", ex.Message);
+            }
+        }
+    }
+}
